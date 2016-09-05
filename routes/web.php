135527438d0a9f1065e41e1a949a32483a5e10f4
Sulaiman <ssk970@gmail.com>
2016-09-05 22:23:12 +0400

    add api/v1/login endpoint
    Required parameters: email, password.
    Response: user_id, api_token

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,29 @@
 <?php
 
+use App\User;
+use Illuminate\Support\Facades\Input;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('api/v1/login', function () {
+    $input = Input::json();
+        
+    $email = $input->get('email');
+    $password = $input->get('password');
+
+    $user = User::where('email', $email)->where('password', md5($password))->first();
+       
+    if (is_null($user)) {
+        return response()->json(array("error" => "invalid credentials"), 401);
+    }
+        
+    return response()->json(array(
+        "user_id" => $user->id,
+        "api_token" => $user->api_token
+    ));
+
+})->middleware('web');
 
 Route::get('api/v1/jokes', 'JokeController@index')->middleware('auth:api');
